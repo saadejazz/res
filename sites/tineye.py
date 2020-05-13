9d03779d625c6341a8355b6110bbf7c6e3f7634a
saadejazz <60652466+saadejazz@users.noreply.github.com>
2020-05-13 19:53:36 +0500

    Update tineye.py

--- sites/tineye.py
+++ sites/tineye.py
@@ -2,7 +2,6 @@ from selenium.common.exceptions import TimeoutException,InvalidArgumentException
 from selenium.webdriver.support import expected_conditions as EC
 from ..utils.utils import efficientGet, dataDict, linkDict
 from selenium.webdriver.support.ui import WebDriverWait
-from ..utils.dat import DATA_DICT, LINK_DICT
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 from ..conf import TIMEOUT
